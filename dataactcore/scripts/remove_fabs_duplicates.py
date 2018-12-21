ad14895532f1e8176245c3b65fa7d6d2eb92c83a
alburde1 <Burdeyny_Alisa@bah.com>
2018-12-21 11:35:49 -0500

    Adding submission clearing to the script

--- dataactcore/scripts/remove_fabs_duplicates.py
+++ dataactcore/scripts/remove_fabs_duplicates.py
@@ -2,6 +2,11 @@ import logging
 
 from dataactcore.interfaces.db import GlobalDB
 from dataactcore.logging import configure_logging
+
+from dataactcore.models.jobModels import CertifiedFilesHistory, CertifyHistory, Submission
+from dataactcore.models.userModel import User  # noqa
+from dataactcore.models.lookups import PUBLISH_STATUS_DICT
+
 from dataactvalidator.health_check import create_app
 
 logger = logging.getLogger(__name__)
@@ -57,4 +62,23 @@ if __name__ == '__main__':
             executed = sess.execute(""" SELECT COUNT(*) as result_count
                                         FROM published_award_financial_assistance
                                         WHERE submission_id = {}""".format(sub))
-        # sess.commit()
+            if executed.fetchone()['result_count'] == 0:
+                cleared_submissions.append(sub)
+
+        # If no submission has been cleared out completely, we can just exit
+        if len(cleared_submissions) == 0:
+            logger.info("No affected submissions have been completely invalidated by the deletes, ending script.")
+            exit(0)
+
+        logger.info("The following submissions have been completely invalidated by the deletes, unpublishing them: " +
+                    ", ".join(str(sub) for sub in cleared_submissions))
+
+        # Unpublish each submission that has been cleared out, including deleting any record of it in the certified
+        # tables
+        for sub in cleared_submissions:
+            sess.query(CertifiedFilesHistory).filter_by(submission_id=sub).delete()
+            sess.query(CertifyHistory).filter_by(submission_id=sub).delete()
+            sess.query(Submission).filter_by(submission_id=sub).\
+                update({"publish_status_id": PUBLISH_STATUS_DICT["unpublished"]})
+        sess.commit()
+        logger.info("Submissions successfully uncertified, script completed.")
