4c0cee7073a06293a903fb464962130fb35c9e10
Daniel Boos <boos_daniel@bah.com>
2019-04-12 13:56:24 -0400

    confirming theory, they are being readded correctly

--- dataactvalidator/app.py
+++ dataactvalidator/app.py
@@ -269,10 +269,10 @@ def cleanup(sig, frame):
         queue = sqs_queue()
 
         for message in current_messages:
-            deleted_message = message.delete()
-            log_to_mount_drive('DELETED MESSAGE:{}'.format(deleted_message))
-            # retried_messaged = retry_message(queue, message)
-            # log_to_mount_drive('RETRIED MESSAGE:{}'.format(retried_messaged))
+            # deleted_message = message.delete()
+            # log_to_mount_drive('DELETED MESSAGE:{}'.format(deleted_message))
+            retried_messaged = retry_message(queue, message)
+            log_to_mount_drive('RETRIED MESSAGE:{}'.format(retried_messaged))
         sys.exit(0)
 
 
