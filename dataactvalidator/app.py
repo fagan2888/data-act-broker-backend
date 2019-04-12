ec745873a8a8d0614175422c4408f51431df7795
Daniel Boos <boos_daniel@bah.com>
2019-04-12 13:15:23 -0400

    Testing the theory that messages are being re-received

--- dataactvalidator/app.py
+++ dataactvalidator/app.py
@@ -271,8 +271,8 @@ def cleanup(sig, frame):
         for message in current_messages:
             deleted_message = message.delete()
             log_to_mount_drive('DELETED MESSAGE:{}'.format(deleted_message))
-            retried_messaged = retry_message(queue, message)
-            log_to_mount_drive('RETRIED MESSAGE:{}'.format(retried_messaged))
+            # retried_messaged = retry_message(queue, message)
+            # log_to_mount_drive('RETRIED MESSAGE:{}'.format(retried_messaged))
         sys.exit(0)
 
 
